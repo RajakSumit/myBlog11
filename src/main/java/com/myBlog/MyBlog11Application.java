4300a99187f9b649a43ad2c9835708e690a49317
Sumit <sumitrajak029@gmail.com>
2024-01-13 14:56:20 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myBlog/MyBlog11Application.java
@@ -0,0 +1,13 @@
+package com.myBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlog11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlog11Application.class, args);
+	}
+
+}
