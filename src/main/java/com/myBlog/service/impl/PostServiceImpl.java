f386b8358cd98cfc90e5d68e5b45ab2144998688
Sumit <sumitrajak029@gmail.com>
2024-01-16 16:50:45 +0530

    127:Create save Post Feature Entity Class Added

---
+++ src/main/java/com/myBlog/service/impl/PostServiceImpl.java
@@ -0,0 +1,34 @@
+package com.myBlog.service.impl;
+
+import com.myBlog.entity.Post;
+import com.myBlog.payload.PostDto;
+import com.myBlog.repository.PostRepository;
+import com.myBlog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savedPost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+        dto.setTitle(savedPost.getTitle());
+        dto.setDescription(savedPost.getDescription());
+        dto.setContent(savedPost.getContent());
+
+
+        return dto;
+    }
+}
